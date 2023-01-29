202377433c0e551320131ee4166efce2d9e45e5b
Adrian Clay <adieclay@gmail.com>
2023-01-29 10:10:27 +0000

    Refactor naming within ParserTest

--- tests/References/ParserTest.php
+++ tests/References/ParserTest.php
@@ -15,51 +15,52 @@ class ParserTest extends TestCase
 
     public function testGetHead()
     {
-        $this->assertRefsHasRef( $this->getReferences( [ References::HEAD => self::TEST_SHA ] ), Parser::HEAD, self::TEST_SHA );
+        $parser = $this->createParserFromGitDir( [ References::HEAD => self::TEST_SHA ] );
+        $this->assertParserHasRef( $parser, Parser::HEAD, self::TEST_SHA );
     }
 
     public function testGetMain()
     {
-        $refs = $this->getReferences( [ 'refs' => [ 'heads' => [ 'main' => self::TEST_SHA ] ] ] );
-        $this->assertRefsHasRef( $refs, self::REF_MAIN, self::TEST_SHA );
+        $parser = $this->createParserFromGitDir( [ 'refs' => [ 'heads' => [ 'main' => self::TEST_SHA ] ] ] );
+        $this->assertParserHasRef( $parser, self::REF_MAIN, self::TEST_SHA );
     }
 
     public function testGetMainWithTrailingNewLine()
     {
-        $refs = $this->getReferences( [ 'refs' => [ 'heads' => [ 'main' => self::TEST_SHA . "\n" ] ] ] );
-        $this->assertRefsHasRef( $refs, self::REF_MAIN, self::TEST_SHA );
+        $parser = $this->createParserFromGitDir( [ 'refs' => [ 'heads' => [ 'main' => self::TEST_SHA . "\n" ] ] ] );
+        $this->assertParserHasRef( $parser, self::REF_MAIN, self::TEST_SHA );
     }
 
     public function testSymbolicRefHead()
     {
-        $refs = $this->getReferences( [ References::HEAD => 'ref: refs/heads/main', 'refs' => [ 'heads' => [ 'main' => self::TEST_SHA ] ] ] );
-        $this->assertRefsHasRef( $refs, References::HEAD, self::TEST_SHA );
+        $parser = $this->createParserFromGitDir( [ References::HEAD => 'ref: refs/heads/main', 'refs' => [ 'heads' => [ 'main' => self::TEST_SHA ] ] ] );
+        $this->assertParserHasRef( $parser, References::HEAD, self::TEST_SHA );
     }
 
     public function testSymbolicRef()
     {
-        $refs = $this->getReferences( [ 'refs' => [ 'heads' => [ 'main' => self::TEST_SHA, 'test' => 'ref: refs/heads/main' ] ] ] );
-        $this->assertRefsHasRef( $refs, 'refs/heads/test', self::TEST_SHA );
+        $refs = $this->createParserFromGitDir( [ 'refs' => [ 'heads' => [ 'main' => self::TEST_SHA, 'test' => 'ref: refs/heads/main' ] ] ] );
+        $this->assertParserHasRef( $refs, self::REF_MAIN, self::TEST_SHA );
     }
 
     public function testGetReferencesReturnsNull()
     {
-        $refs = $this->getReferences( [ 'refs' => [] ] );
-        $this->assertNull($refs->getReference('fake_ref'));
+        $parser = $this->createParserFromGitDir( [ 'refs' => [] ] );
+        $this->assertNull($parser->getReference('fake_ref'));
     }
 
     public function testPackedRef()
     {
-        $refs = $this->getReferences( [ 'packed-refs' => self::TEST_SHA . ' ' . self::REF_MAIN ] );
-        $this->assertRefsHasRef( $refs, self::REF_MAIN, self::TEST_SHA );
+        $parser = $this->createParserFromGitDir( [ 'packed-refs' => self::TEST_SHA . ' ' . self::REF_MAIN ] );
+        $this->assertParserHasRef( $parser, self::REF_MAIN, self::TEST_SHA );
     }
 
-    public function testStalePackedRef()
+    public function testDoesNotUsePackedRefValueIfDefinedWithinReferences()
     {
-        $dodgyRef = 'DEADDEADDEADDEADDEADDEADDEADDEAD';
-        $refs = $this->getReferences( [ References::HEAD => self::TEST_SHA, 'packed-refs' => $dodgyRef . ' ' . References::HEAD ] );
-        foreach( $refs as $ref ) {
-            $this->assertNotEquals( $dodgyRef, $ref->getSHA() );
+        $packedRefValue = 'DEADDEADDEADDEADDEADDEADDEADDEAD';
+        $parser = $this->createParserFromGitDir( [ References::HEAD => self::TEST_SHA, 'packed-refs' => $packedRefValue . ' ' . References::HEAD ] );
+        foreach( $parser as $ref ) {
+            $this->assertNotEquals( $packedRefValue, $ref->getSHA() );
         }
     }
 
@@ -67,23 +68,19 @@ class ParserTest extends TestCase
     {
         $packedRefs  = "# pack-refs with: peeled\n";
         $packedRefs .= self::TEST_SHA . ' ' . self::REF_MAIN;
-        $refs = $this->getReferences( [ 'packed-refs' => $packedRefs ] );
-        $this->assertRefsHasRef( $refs, self::REF_MAIN, self::TEST_SHA );
-        $this->assertCount( 1, $refs->getIterator() );
+        $parser = $this->createParserFromGitDir( [ 'packed-refs' => $packedRefs ] );
+        $this->assertParserHasRef( $parser, self::REF_MAIN, self::TEST_SHA );
+        $this->assertCount( 1, $parser->getIterator() );
     }
 
     public function testPackedRefFileWithTrailingNewLine()
     {
-        $refs = $this->getReferences( [ 'packed-refs' => self::TEST_SHA . ' ' . self::REF_MAIN . "\n" ] );
-        $this->assertRefsHasRef( $refs, self::REF_MAIN, self::TEST_SHA );
-        $this->assertCount( 1, $refs->getIterator() );
+        $parser = $this->createParserFromGitDir( [ 'packed-refs' => self::TEST_SHA . ' ' . self::REF_MAIN . "\n" ] );
+        $this->assertParserHasRef( $parser, self::REF_MAIN, self::TEST_SHA );
+        $this->assertCount( 1, $parser->getIterator() );
     }
 
-    /**
-     * @param array $gitDir
-     * @return Parser
-     */
-    private function getReferences( array $gitDir )
+    private function createParserFromGitDir( array $gitDir ): Parser
     {
         vfsStream::setup( 'root', null, [ '.git' => $gitDir ] );
         $repo = new Repository( vfsStream::url( 'root' ) );
@@ -92,12 +89,7 @@ class ParserTest extends TestCase
         return $refs;
     }
 
-    /**
-     * @param Parser $refs
-     * @param string $ref
-     * @param string $sha
-     */
-    public function assertRefsHasRef( Parser $refs, $ref, $sha )
+    public function assertParserHasRef( Parser $refs, string $ref, string $sha )
     {
         $refsArray = iterator_to_array( $refs, true );
         $this->assertArrayHasKey( $ref, $refsArray );
